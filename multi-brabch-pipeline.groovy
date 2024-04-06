b5fdd3cb0cf61ab75de522b32cdd71309425d139
asha43215678 <164387880+asha43215678@users.noreply.github.com>
2024-04-07 00:20:30 +0530

    Update multi-brabch-pipeline.groovy

--- multi-brabch-pipeline.groovy
+++ multi-brabch-pipeline.groovy
@@ -1,1 +1,40 @@
+folder('multi-seed') {
+    displayName('multi-seed')
+    description('Folder for multi-sed')
+}
 
+folder('multi-seed/application') {
+  multibranchPipelineJob('Microservice/application/sample-app') {
+    branchSources {
+      branchSource {
+        source {
+         git {
+          id('sample-app')
+          remote('https://github.com/asha43215678/project2')
+          }
+        }
+        strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                       }
+                  }
+             }
+         }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('Jenkinsfile')
+            }
+        }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+     }
+   }
+}
